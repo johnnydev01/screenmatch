86dccfa43dc6f72c34add1ac536399b3efbbc7e3
Johnny Silva <johnnysilva.dev@gmail.com>
2025-03-15 11:54:02 -0300

    feat: aula-3

---
+++ src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
@@ -0,0 +1,26 @@
+package br.com.alura.screenmatch.calculos;
+
+import br.com.alura.screenmatch.modelos.Titulo;
+
+public class CalculadoraDeTempo {
+
+    private int tempoTotal = 0;
+
+
+    public int getTempoTotal() {
+        return this.tempoTotal;
+    }
+
+//    public void inclui(Filme filme) {
+//        this.tempoTotal += filme.getDuracaoEmMinutos();
+//    }
+//
+//    public void inclui(Serie serie) {
+//        this.tempoTotal += serie.getDuracaoEmMinutos();
+//    }
+
+
+    public void inclui(Titulo titulo) {
+        this.tempoTotal += titulo.getDuracaoEmMinutos();
+    }
+}
