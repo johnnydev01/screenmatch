178f54aec8a418a863a528baa87a4e711cf1d0bb
Johnny Silva <johnnysilva.dev@gmail.com>
2025-03-15 11:09:42 -0300

    feat: aula-2

--- src/Principal.java
+++ src/Principal.java
@@ -1,20 +1,22 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
 
-        meuFilme.nome = "O Poderoso chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("O Poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
 
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+//        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
     }
 }
