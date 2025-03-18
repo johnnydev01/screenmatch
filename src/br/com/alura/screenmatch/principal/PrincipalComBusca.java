a2943a6d81a92d1dea2cd0460505ed7a87c3fcec
Johnny Silva <johnnysilva.dev@gmail.com>
2025-03-18 11:52:29 -0300

    feat: aula-11

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -0,0 +1,30 @@
+package br.com.alura.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o filme que você deseja pesquisar: ");
+        String busca = scanner.nextLine();
+
+        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=f3778bb5";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
